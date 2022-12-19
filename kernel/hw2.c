e6a2b08f919196db2c816f26c8a6c7dc005dc8cf
Tom Waknine <tomtiger6@gmail.com>
2022-12-19 13:21:24 +0000

    tset

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -9,6 +9,7 @@ asmlinkage long sys_hello(void)
 
 asmlinkage long  sys_set_weight(int num) 
 {
+    printk("set weight\n");
     if (num < 0)
       return -EINVAL;
     current->weight =num;
@@ -17,26 +18,35 @@ asmlinkage long  sys_set_weight(int num)
 
 asmlinkage long  sys_get_weight(void) 
 {
-    return current->weight;
+  printk("get weight\n");
+  return current->weight;
 }
 
 asmlinkage long  sys_get_leaf_children_sum(void) 
 {
-    int sum =0;
-    struct task_struct *task;
-    if (list_empty(&current->children)) 
-        {
-            return -ECHILD;
-        }
-    list_for_each_entry(task, &current->children, sibling) {
-      if (list_empty(&task->children))
-         sum += task->weight;
-      else
-         sum += sys_get_leaf_children_sum();
-   }
+  int sum =0;
+  struct task_struct *task;        
+  printk("1enter sum+task.weight\n");
 
-   return sum;
-
+  if (list_empty(&current->children)) 
+      {
+          return -ECHILD;
+      }
+  list_for_each_entry(task, &current->children,sibling) 
+  {
+    if (list_empty(&task->children))
+    {
+      printk("sum+task.weight\n");
+      sum += task->weight;
+    }
+    else
+    { 
+      printk("get_leaf_child\n");
+      sum += sys_get_leaf_children_sum();
+      printk("leaf_child worked\n");
+    }
+  }
+  return sum;
 }
 
 asmlinkage long sys_get_heaviest_ancestor(void)
@@ -45,10 +55,12 @@ asmlinkage long sys_get_heaviest_ancestor(void)
    struct task_struct *task = current;
    int ret_pid= current->pid;
    int best_weight=current->weight;
-    do
+   printk("ENTER heaviest\n");
+   do
     {
         if (task->weight > best_weight)
          {
+         printk("update heaviest\n");
          ret_pid=task->pid;
          best_weight=task->weight;
          }
